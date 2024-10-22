01fdd04e60bcf368f13c871b90a85efce217ae6c
Stepness <stefanodinucci@gmail.com>
2024-10-22 19:17:07 +0200

    REPL

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"token/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Money programming language!\n", user.Username)
+	fmt.Printf("Start typing\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
