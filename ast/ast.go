9d747f9fd270c74b3ed0df1f1394081879e9d2f0
Stepness <stefanodinucci@gmail.com>
2024-11-11 22:49:51 +0100

    Parse boolean and grouping expression

--- ast/ast.go
+++ ast/ast.go
@@ -63,6 +63,11 @@ type IntegerLiteral struct {
 	Value int64
 }
 
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -158,3 +163,7 @@ func (ie *InfixExpression) String() string {
 	out.WriteString(")")
 	return out.String()
 }
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
