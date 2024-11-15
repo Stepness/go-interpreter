30e47b875f58d783e55ea08aaf75fc22560c52b5
Stepness <stefanodinucci@gmail.com>
2024-11-15 22:05:38 +0100

    Fix function evaluation

--- parser/parser.go
+++ parser/parser.go
@@ -357,22 +357,28 @@ func (p *Parser) parseFunctionLiteral() ast.Expression {
 
 func (p *Parser) parseFunctionParameters() []*ast.Identifier {
 	var identifiers []*ast.Identifier
+
 	if p.peekTokenIs(token.RPAREN) {
 		p.nextToken()
 		return identifiers
 	}
+
 	p.nextToken()
+
 	ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 	identifiers = append(identifiers, ident)
+
 	for p.peekTokenIs(token.COMMA) {
 		p.nextToken()
 		p.nextToken()
 		ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 		identifiers = append(identifiers, ident)
-		if !p.expectPeek(token.RPAREN) {
-			return nil
-		}
 	}
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
 	return identifiers
 }
 
